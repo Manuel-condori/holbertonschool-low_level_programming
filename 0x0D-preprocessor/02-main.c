d2494fd2c442e57423cc03e45fe6b2146183da77
Manuel-condori <2828@holbertonschool.com>
2021-03-08 23:18:35 +0000

    preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef FILE
+	#define FILE printf("%s\n", __FILE__);
+#endif
+
+int main(void)
+{
+	FILE
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#ifndef FILE
+#define FILE printf("%s\n", __FILE__)
+#endif
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	FILE;
+	return (0);
+}
