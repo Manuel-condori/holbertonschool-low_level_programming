757d49180faa18eff9601066f8cdc269a4e10721
Manuel-condori <2828@holbertonschool.com>
2021-03-09 03:47:22 +0000

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef _DOG_H
 #define _DOG_H
 /**
- * struct dog - struct for dog info
+ * struct dog - struct for dog
  * @name: type char
  * @age: float type
  * @owner: char type
@@ -12,4 +12,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * prototypes
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
