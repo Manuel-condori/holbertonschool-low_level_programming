37e14263da0a9a857bec6ea1976731330c845e24
Manuel-condori <2828@holbertonschool.com>
2021-03-21 00:00:47 +0000

    advanced 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-/*
- * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- * return: 1
+#include <unistd.h>
+/**
+ * main - prints string,followed by a new line, to the standard error.
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
