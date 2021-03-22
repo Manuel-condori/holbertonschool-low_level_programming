fb8538932e9d663db9c9324915119e8525bfa4ea
Manuel-condori <2828@holbertonschool.com>
2021-03-22 05:23:17 +0000

    advanced 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,47 @@
 #include "holberton.h"
 /**
- * print_times_table - prints the n times table, starting with 0.
- * @n: tipo int.
+ * print_times_table - takes an input and prints out the times
+ * @n: int type
+ * Return: return table of values
  */
+
 void print_times_table(int n)
 {
 	int a, b;
 
 	if (n < 15 || n < 0)
-	{
 
-		a = 0;
-
-		while (a <= n)
+		for (a = 0; a <= n; a++)
 		{
-			b = 0;
-				while (b <= n)
+			for (b = 0; b <= n; b++)
+			{
+				if (b == 0)
+					_putchar(((a * b) % 10) + '0');
+				else if ((a * b) <= 9)
 				{
-					if (b == 0)
-					{
-						_putchar(((a * b) % 10) + '0');
-					}
-					else if ((a * b) <= 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((a * b) + '0');
-					}
-					else if ((a * b) <= 99)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(((a * b) / 10) + '0');
-						_putchar(((a * b) % 10) + '0');
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(((a * b) / 100) + '0');
-						_putchar((((a * b) / 10) % 10) + '0');
-						_putchar(((a * b) % 10) + '0');
-					}
-				b++;
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((a * b) + '0');
 				}
-		a++;
-		_putchar('\n');
+				else if ((a * b) <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((a * b) / 10) + '0');
+					_putchar(((a * b) % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((a * b) / 100) + '0');
+					_putchar((((a * b) / 10) % 10) + '0');
+					_putchar(((a * b) % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-	}
 }
