987b4c2e213340abbc22315d1288402af77260aa
Manuel-condori <2828@holbertonschool.com>
2021-03-22 07:34:22 +0000

    advanced 3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,14 +6,18 @@
 int main(void)
 {
 	int i;
-	int x = 0, y = 1, z = 1;
+	long int x = 0, y = 1, z = 1;
 	
 	for (i = 1; i <= 50; i++)
 	{
 		z = x + y;
 		x = y;
 		y = z;
-		printf("%i, ",z);
+		printf("%li",z);
+		if(i < 50)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 	return (0);
