e0f5c9eed333a4d4f6c8cd559c0479ceb9fcf08a
Manuel-condori <2828@holbertonschool.com>
2021-04-03 23:04:15 +0000

    advanced 2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,38 +5,15 @@
  */
 void print_number(int n)
 {
-	if (n > 9 && n < 100)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n > 99 && n < 1000)
-	{
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 + 8);
-		_putchar(n % 10 + '0');
-	}
-	else if (n > 999 && n < 10000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar(n / 100 - 30);
-		_putchar(n / 10 - 52);
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 0)
-	{
-		_putchar(n + '0');
-	}
-	else if (n < 0 && n > -10)
-	{
-		_putchar('-');
-		_putchar(n + '0');
 
-	}
-	else if (n < -9 && n > -100)
+	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(n / 10 + 'B');
-		_putchar(n % 10 + '@');
+		n = -n;
 	}
+
+	if ((n / 10) > 0)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
 }
