8a05436a25e7f731f6243323ade9913997326d64
Manuel-condori <2828@holbertonschool.com>
2021-04-05 04:25:59 +0000

    advanced 2

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,26 @@
 #include "holberton.h"
-
+/**
+ * _atoi - convert a string to an integer
+ * @s: type char
+ * Return: integer
+ */
 int _atoi(char *s)
 {
-        int signo = 1;
-        unsigned int num = 0;
+	int signo = 1;
+	unsigned int num = 0;
 
-        do {
-                if (*s == '-')
-                {
-                        signo *= -1;
-                }
-                else if (*s >= '0' && *s <= '9')
-                {
-                        num = (num * 10) + (*s - '0');
-                }
-                else if (num > 0)
-                        break;
-        }while (*s++);
+	do {
+		if (*s == '-')
+		{
+			signo *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-        return (num * signo);
+	return (num * signo);
 }
