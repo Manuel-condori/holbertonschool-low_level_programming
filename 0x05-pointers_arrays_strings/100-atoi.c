0b142e3bf4e1aec4f4f882c19589458bee6ed9cf
Manuel-condori <2828@holbertonschool.com>
2021-04-05 01:10:53 +0000

    advanced 1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+int _atoi(char *s)
+{
+        int signo = 1;
+        unsigned int num = 0;
+
+        do {
+                if (*s == '-')
+                {
+                        signo *= -1;
+                }
+                else if (*s >= '0' && *s <= '9')
+                {
+                        num = (num * 10) + (*s - '0');
+                }
+                else if (num > 0)
+                        break;
+        }while (*s++);
+
+        return (num * signo);
+}
