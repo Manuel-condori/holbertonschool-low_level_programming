14f860cea306a16049f5b9ff8cca9eb8ba1ccc5a
Manuel-condori <2828@holbertonschool.com>
2021-10-22 01:55:57 -0500

    first commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Entry point
+ * @width: s
+ * @height: s
+ *
+ * Return: Always 0 (Success)
+ */
+int **alloc_grid(int width, int height)
+{
+int **array, i, j;
+
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+array = (int **)malloc(height * sizeof(int *));
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+array[i] = (int *)malloc(width * sizeof(int));
+if (array[i] == NULL)
+{
+for (i = i - 1; i >= 0; i--)
+{
+free(array[i]);
+}
+free(array);
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+array[i][j] = 0;
+}
+}
+return (array);
+}
